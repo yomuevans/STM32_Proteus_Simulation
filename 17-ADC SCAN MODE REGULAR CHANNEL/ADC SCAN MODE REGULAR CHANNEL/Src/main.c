db955f470baaf9f2e805df40bb0bfe51a579ad22
yomuevans <54433381+yomuevans@users.noreply.github.com>
2025-07-20 09:07:10 +0300

    Update main.c

--- 17-ADC SCAN MODE REGULAR CHANNEL/ADC SCAN MODE REGULAR CHANNEL/Src/main.c
+++ 17-ADC SCAN MODE REGULAR CHANNEL/ADC SCAN MODE REGULAR CHANNEL/Src/main.c
@@ -1,12 +1,15 @@
 #include "main.h"
-#include<stdio.h>
-#include<string.h>
-
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 ADC_HandleTypeDef hadc1;
 DMA_HandleTypeDef hdma_adc1;
 UART_HandleTypeDef huart1;
 
+char MSG[64];
+uint16_t ADC_VAL[2];
+volatile int adcConvertionComplete = 0;
 
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
@@ -14,42 +17,48 @@ static void MX_DMA_Init(void);
 static void MX_ADC1_Init(void);
 static void MX_USART1_UART_Init(void);
 
-
-char MSG[50];
-uint16_t ADC_VAL[2];
-volatile int adcConvertionComplete=0;
-
-
 void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc)
 {
-  adcConvertionComplete=1;
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, 0);
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, 1);
+    adcConvertionComplete = 1;
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_SET);
 }
-
 
 int main(void)
 {
-  HAL_Init();
-  SystemClock_Config();
-  MX_GPIO_Init();
-  MX_DMA_Init();
-  MX_ADC1_Init();
-  MX_USART1_UART_Init();
+    HAL_Init();
+    SystemClock_Config();
+    MX_GPIO_Init();
+    MX_DMA_Init();
+    MX_ADC1_Init();
+    MX_USART1_UART_Init();
+    HAL_ADCEx_Calibration_Start(&hadc1);
+
+    while (1)
+    {
+    	HAL_ADC_Start_DMA(&hadc1, (uint32_t*)ADC_VAL, 2);
+    	while (adcConvertionComplete == 0) {}
+    	adcConvertionComplete = 0;
+
+    	uint32_t v1_mV = (ADC_VAL[0] * 3300) / 4096;
+    	uint32_t v2_mV = (ADC_VAL[1] * 3300) / 4096;
+
+    	snprintf(MSG, sizeof(MSG), "V1 = %lu mV (%u)\r\n", v1_mV, ADC_VAL[0]);
+    	HAL_UART_Transmit(&huart1, (uint8_t*)MSG, strlen(MSG), HAL_MAX_DELAY);
+
+    	snprintf(MSG, sizeof(MSG), "V2 = %lu mV (%u)\r\n", v2_mV, ADC_VAL[1]);
+    	HAL_UART_Transmit(&huart1, (uint8_t*)MSG, strlen(MSG), HAL_MAX_DELAY);
+
+    	HAL_ADC_Stop(&hadc1);
 
 
-  while (1)
-  {
+        //HAL_UART_Transmit_DMA(&huart1, (uint8_t*)MSG, len);
 
-	HAL_ADC_Start_DMA(&hadc1, (uint32_t*)ADC_VAL, 2);
-    while(adcConvertionComplete==0){}
-    adcConvertionComplete==0;
-    snprintf(MSG,sizeof(MSG),"ADC SCAN Channels =  %d\n\r",ADC_VAL[0]);
-    HAL_UART_Transmit(&huart1, MSG, strlen(MSG), HAL_MAX_DELAY);
-    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, 1);
-    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, 0);
-    HAL_Delay(1000);
-  }
+        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_SET);
+        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_RESET);
+
+        HAL_Delay(1000);
+    }
 }
 
 
@@ -94,13 +103,24 @@ static void MX_ADC1_Init(void)
   hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
   hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
   hadc1.Init.NbrOfConversion = 2;
+
+  // ⚠️ CRITICAL: Enable DMA Continuous Requests
+  hadc1.DMA_Handle->Init.Mode = DMA_CIRCULAR;     // Circular buffer mode
+  hadc1.DMA_Handle->Init.PeriphInc = DMA_PINC_DISABLE; // ADC is fixed address
+  hadc1.DMA_Handle->Init.MemInc = DMA_MINC_ENABLE;     // Memory increments
+  hadc1.DMA_Handle->Init.PeriphDataAlignment = DMA_PDATAALIGN_HALFWORD; // 16-bit ADC
+  hadc1.DMA_Handle->Init.MemDataAlignment = DMA_MDATAALIGN_HALFWORD;    // 16-bit buffer
+  hadc1.DMA_Handle->Init.Priority = DMA_PRIORITY_HIGH;  // High priority
+
+
+  //hadc1.Init.EOCSelection = ADC_EOC_SEQ_CONV; // <- CRITICAL
   if (HAL_ADC_Init(&hadc1) != HAL_OK)
   {
     Error_Handler();
   }
   sConfig.Channel = ADC_CHANNEL_8;
   sConfig.Rank = ADC_REGULAR_RANK_1;
-  sConfig.SamplingTime = ADC_SAMPLETIME_1CYCLE_5;
+  sConfig.SamplingTime =ADC_SAMPLETIME_239CYCLES_5;
   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
   {
     Error_Handler();
@@ -130,20 +150,42 @@ static void MX_USART1_UART_Init(void)
 static void MX_DMA_Init(void) 
 {
   __HAL_RCC_DMA1_CLK_ENABLE();
+
+  hdma_adc1.Instance = DMA1_Channel1;
+  hdma_adc1.Init.Direction = DMA_PERIPH_TO_MEMORY;
+  hdma_adc1.Init.PeriphInc = DMA_PINC_DISABLE;
+  hdma_adc1.Init.MemInc = DMA_MINC_ENABLE;
+  hdma_adc1.Init.PeriphDataAlignment = DMA_PDATAALIGN_HALFWORD;
+  hdma_adc1.Init.MemDataAlignment = DMA_MDATAALIGN_HALFWORD;
+  hdma_adc1.Init.Mode = DMA_CIRCULAR;
+  hdma_adc1.Init.Priority = DMA_PRIORITY_HIGH;
+
+  HAL_DMA_Init(&hdma_adc1);
+  __HAL_LINKDMA(&hadc1, DMA_Handle, hdma_adc1);
+
   HAL_NVIC_SetPriority(DMA1_Channel1_IRQn, 0, 0);
   HAL_NVIC_EnableIRQ(DMA1_Channel1_IRQn);
 }
 static void MX_GPIO_Init(void)
 {
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
-  __HAL_RCC_GPIOA_CLK_ENABLE();
-  __HAL_RCC_GPIOB_CLK_ENABLE();
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1|GPIO_PIN_2, GPIO_PIN_RESET);
-  GPIO_InitStruct.Pin = GPIO_PIN_1|GPIO_PIN_2;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+
+    // Configure PB0 (ADC Channel 8) and PB1 (ADC Channel 9) as ANALOG INPUTS
+    GPIO_InitStruct.Pin = GPIO_PIN_0 | GPIO_PIN_1;  // PB0 and PB1
+    GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+    // Configure PA1 and PA2 as LED OUTPUTS (digital only)
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1 | GPIO_PIN_2, GPIO_PIN_RESET);
+    GPIO_InitStruct.Pin = GPIO_PIN_1 | GPIO_PIN_2;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 }
 void Error_Handler(void)
 {
